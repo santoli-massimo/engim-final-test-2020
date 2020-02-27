4677c55732760a5db50f25860771f7623ecbbccf
stefaniaC87 <stefaniacappellino@gmail.com>
2020-02-27 23:20:34 +0100

    aggiunto codice js calcolatrice

--- main.js
+++ main.js
@@ -16,3 +16,25 @@ santoli15.init(); // inizializza il giochino/dedica in home
 
 francescoInit();
 
+var tasti_ste = document.getElementsByClassName('numeri_ste');
+var uguale_ste = document.getElementById('uguale_ste');
+var c_ste = document.getElementById('c_ste');
+var i_ste = tasti_ste.length;
+
+while(i_ste--){
+    tasto_ste = tasti_ste[i_ste];
+    tasto_ste.onclick=function(){
+    inserisci_ste(this.value);
+  
+}
+
+  
+}
+uguale_ste.onclick=calcola_ste;
+c_ste.onclick=cancella_ste;
+
+var colore_ste;
+var colore_label_ste;
+var colorebase_ste='#ff3399';
+window.addEventListener('load', selezionecolore_ste, false);
+
